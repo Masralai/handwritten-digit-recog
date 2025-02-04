2a242ac3e84ef5cdce8f06cc031d1a454adb8f66
Devdeep Paul <154480206+Masralai@users.noreply.github.com>
2025-02-04 22:03:35 +0530

    Update FCNN.py

--- alt/FCNN.py
+++ alt/FCNN.py
@@ -8,13 +8,10 @@ import tensorflow as tf
 # preprocessing
 #--------------------
 
-
 mnist = tf.keras.datasets.mnist
-# x->pixel data or image itself , y-> classifcation
 (x_train, y_train), (x_test,y_test) = mnist.load_data()
 
-# we will scale down from 0-255 to 
-#we will normalize the pixels and not the digits as it makes it easier for teh NNs to do ythe calcs
+
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
@@ -23,17 +20,14 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 #--------------------
 
 model = tf.keras.models.Sequential()
-#flatten will convert the 28x28 grid into a line of pixels
+
 model.add(tf.keras.layers.Flatten(input_shape=(28,28))) 
 model.add(tf.keras.layers.Dense(128,activation='relu'))
 model.add(tf.keras.layers.Dense(128,activation='relu'))
 model.add(tf.keras.layers.Dense(10,activation='softmax'))
-#softmax makes sure all the 10 neurons add up to 1
-#softmax gives probablity for each digit to be the right answer
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-# sparse_categorical_crossentropy is used because the task is a multi-class classification (digits 0-9)
-# adam is a optimizer that adjusts the learning rate during training.
+
 
 #--------------------
 # training
@@ -42,7 +36,6 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=[
 model.fit(x_train, y_train , epochs=100)
 
 model.save('digit-recog-model.keras')
-
 
 model=tf.keras.models.load_model('digit-recog-model.keras')
 
@@ -56,10 +49,9 @@ image_number =1
 while os.path.isfile(f"digits/digit_{image_number}.png"):
     try:
         img = cv2.imread(f"digits/digit_{image_number}.png")[:,:,0]
-        img = np.invert(np.array([img])) #inverting as image is white on black and not vice versa
+        img = np.invert(np.array([img])) 
         prediction = model.predict(img)
-        #np.argmax() gives us the index of the field with highest number 
-        #basically the neuron with the hightest activation
+       
         print(f"this digit is might be {np.argmax(prediction)}")
         plt.imshow(img[0], cmap=plt.cm.binary)
         plt.show()
